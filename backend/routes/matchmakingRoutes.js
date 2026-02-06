220ce7dc4b570f406ca9c1b758292dfff3db5ed1
Pasindu Dissanayake <pasindudissanayake035@gmail.com>
2026-02-06 21:03:02 +0530

    Fix conflict

--- backend/routes/matchmakingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { findOpponents } = require('../controllers/matchmakingController');
-
-// POST http://localhost:5000/api/matchmaking/find
-router.post('/find', findOpponents);
-
-module.exports = router;
