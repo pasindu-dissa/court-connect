aab0f0bae75975a1f61bcf9a3cbd760fcafc6fb2
yashmikavisha1-byte <yashmikavisha1@gmail.com>
2026-03-15 18:04:55 +0530

    Update Linux plugin registrant
    Regenerated/updated Linux plugin registration file (likely due to dependency changes or pub get)
    Contains updated plugin registration list / line ending normalization

--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus
 import file_selector_macos
 import firebase_auth
 import firebase_core
@@ -14,7 +13,6 @@ import google_sign_in_ios
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
