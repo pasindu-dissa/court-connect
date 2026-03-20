592630b12a5df33a697dde7a626d8f833a2b92ad
sanuriahangamage <sanuri.20241440@iit.ac.lk>
2026-03-20 12:57:38 +0530

    Update bookingRoutes.js
     Add getUserBookings route to bookingRoutes

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createBooking, getCourtBookings } = require('../controllers/bookingController');
+const { createBooking, getCourtBookings, getUserBookings } = require('../controllers/bookingController');
 
 router.post('/', createBooking);
+router.get('/user/:userId', getUserBookings);
 router.get('/:courtId', getCourtBookings);
 
 module.exports = router;
