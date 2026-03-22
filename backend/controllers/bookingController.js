e5122361dc6376e7a8e869a39952275295212d26
Isula Rajinda Illeperuma <isulaillepe2024@gmail.com>
2026-03-23 00:49:23 +0530

    fix: Corrected Booking model import casing and updated

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,4 +1,4 @@
-const Booking = require('../models/Booking');
+const Booking = require('../models/booking');
 const Court = require('../models/court');
 const User = require('../models/User');
 const { pushNotificationToUser } = require('../services/notificationService');
